56b049768ccbc848474f69616df842c309fd6a8d
Rah-Olin <qiancuijia@gmail.com>
2017-07-21 02:09:45 -0400

    read from file

--- server.js
+++ server.js
@@ -7,13 +7,22 @@ var fs = require("fs")
 
 
 //Store all JS and CSS in Scripts folder.
-
 app.listen(3000);
+
+
 
 app.get('/',function(req,res){
   res.sendFile(__dirname + "/public/index.html");
   //__dirname : It will resolve to your project folder.
 });
+
+app.get('/messages', function (req, res) {
+   fs.readFile( __dirname + "/" + "public/message.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+});
+
 app.use(express.static(path.join(__dirname, '/public')));
 
 
