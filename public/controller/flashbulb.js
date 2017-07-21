c1d7e0eafb804593989a4aa99375ba9cb39c69f5
Rah-Olin <qiancuijia@gmail.com>
2017-07-21 01:20:18 -0400

    change route

--- public/controller/flashbulb.js
+++ public/controller/flashbulb.js
@@ -1,4 +1,5 @@
 var app = angular.module("flashbulb",[]);
+
 app.controller("flashbulbCtrl",function($scope, $http) {
 	$scope.messages = 
 	[
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-var http = require('http');
 
-http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
-    
-}).listen(process.env.PORT || 8080);
+
+var express = require("express");
+var app     = express();
+var path    = require("path");
+var fs = require("fs")
+
+
+//Store all JS and CSS in Scripts folder.
+
+app.listen(3000);
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname + "/public/index.html");
+  //__dirname : It will resolve to your project folder.
+});
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+console.log("Running at Port 3000");
+
+module.exports = app;
